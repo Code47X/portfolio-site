2be98cbcc99e5377ccf36907eb98ed8105a557db
Tanner Hartwig <tanner.hartwig57@gmail.com>
2019-07-31 21:47:24 -0500

    breakpoints.js

---
+++ src/utility/breakpoints.js
@@ -0,0 +1,13 @@
+const breakpoints = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px'
+}
+
+export const device = {
+  mobile: `(min-width: ${breakpoints.sm})`,
+  tablet: `(min-width: ${breakpoints.md})`,
+  laptop: `(min-width: ${breakpoints.lg})`,
+  desktop: `(min-width: ${breakpoints.xl})`,
+};
